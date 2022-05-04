9faa8bdaea954c33cdb86c67bf726b796801b6cd
Salim Karim <imsjk8@gmail.com>
2022-05-04 12:30:54 +1000

    added Onlogs example

---
+++ getBalance.js
@@ -0,0 +1,19 @@
+import { struct, u32, ns64 } from "@solana/buffer-layout";
+import { Buffer } from 'buffer';
+import { Keypair, Connection, clusterApiUrl, LAMPORTS_PER_SOL, Transaction, TransactionInstruction, SystemProgram, sendAndConfirmTransaction } from "@solana/web3.js";
+import bs58 from 'bs58';
+import fs from 'fs';
+
+
+let paySecret = fs.readFileSync('devkey-pay.json');
+let payJSON = JSON.parse(paySecret);
+let payer = Keypair.fromSecretKey(Uint8Array.from(payJSON))
+
+console.log('payer',payer.publicKey.toBase58())
+
+let connection = new Connection(clusterApiUrl('devnet'));
+
+let balance = await connection.getBalance(
+  payer.publicKey
+);
+console.log('balance = ', balance)
---
+++ getLargestAccounts.js
@@ -0,0 +1,17 @@
+import { struct, u32, ns64 } from "@solana/buffer-layout";
+import { Buffer } from 'buffer';
+import { Keypair, Connection, clusterApiUrl, LAMPORTS_PER_SOL, Transaction, TransactionInstruction, SystemProgram, sendAndConfirmTransaction } from "@solana/web3.js";
+import bs58 from 'bs58';
+import fs from 'fs';
+
+
+let paySecret = fs.readFileSync('devkey-pay.json');
+let payJSON = JSON.parse(paySecret);
+let payer = Keypair.fromSecretKey(Uint8Array.from(payJSON))
+
+console.log('payer',payer.publicKey.toBase58())
+
+let connection = new Connection(clusterApiUrl('mainnet-beta'));
+
+let balance = await connection.getLargestAccounts();
+console.log('balance = ', balance)
